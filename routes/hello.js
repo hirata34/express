43c583b3a20a3228d8757b4820ae40bbd3abd699
hirata34 <ht112516neko@gmail.com>
2025-05-22 03:01:09 +0900

    add hello page

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'express 2' });
+  res.render('hello', { title: 'Hello express' });
 });
 
 module.exports = router;
